06964a3bec80be9658efd55b9a6c6b530ea608ac
Emilia-Victoria <emiliav.helsted@gmail.com>
2022-09-08 17:45:23 +0200

    Tested and implemented IsLeapYear

--- MyApp/MyApp.Tests/UnitTest1.cs
+++ MyApp/MyApp.Tests/UnitTest1.cs
@@ -1,10 +1,28 @@
+using Xunit;
+using System;
 namespace MyApp.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void isleapYearTrue()
     {
+        //Arrange
+        var p = new Program();
+        //Act
+        bool result = p.IsLeapYear(20);
+        //Assert
+        Assert.True(result);
+    }
 
+    [Fact]
+    public void isleapYearFalse()
+    {
+        //Arrange
+        var p = new Program();
+        //Act
+        bool result = p.IsLeapYear(19);
+        //Assert
+        Assert.False(result);
     }
 }
